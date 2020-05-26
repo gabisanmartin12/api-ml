89748b7027a5ee1d724beae66d78be17038c624d
Gabriel San Martin <gabisanmartin12@gmail.com>
2020-05-26 11:19:58 -0300

    Fix wrong parsing middleware.

--- middlewares/items.js
+++ middlewares/items.js
@@ -4,20 +4,19 @@ const { Item } = require("../models/item");
  * Middleware to parse all the items to the correct format
  */
 module.exports.parseItems = (req, res, next) => {
+  let categories =
+    req.data.categories &&
+    req.data.categories.values &&
+    req.data.categories.values.length &&
+    req.data.categories.values[0].path_from_root &&
+    req.data.categories.values[0].path_from_root.length
+      ? req.data.categories.values[0].path_from_root.map((cat) => cat.name)
+      : [];
+
   req.data = {
     ...req.data,
-    ...req.data.items.reduce(
-      (result, item) => {
-        if (!result.categories.includes(item.category_id))
-          result.categories.push(item.category_id);
-        result.items.push(new Item({ ...item }));
-        return result;
-      },
-      {
-        categories: [],
-        items: [],
-      }
-    ),
+    items: req.data.items.map((item) => new Item({ ...item })),
+    categories: categories,
   };
   next();
 };
@@ -26,11 +25,16 @@ module.exports.parseItems = (req, res, next) => {
  * Middleware to parse the item to the correct format
  */
 module.exports.parseItem = (req, res, next) => {
+  let item = new Item({ ...req.data.item })
+    .setSoldQuantity(req.data.item.sold_quantity)
+    .setDescription(req.data.item.description);
+
+  if (req.data.item.pictures.length)
+    item.setPicture(req.data.item.pictures[0].secure_url);
+
   req.data = {
     ...req.data,
-    item: new Item({ ...req.data.item })
-      .setSoldQuantity(req.data.item.sold_quantity)
-      .setDescription(req.data.item.description),
+    item: item,
   };
   next();
 };
