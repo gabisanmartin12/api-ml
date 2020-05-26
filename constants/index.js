bbf0c02cbd1bbac1fdecce39b890bd78345d8496
Gabriel San Martin <gabisanmartin12@gmail.com>
2020-05-26 11:14:42 -0300

    Add and configure CORS.

--- constants/index.js
+++ constants/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { URLS } = require("./urls");
 
 module.exports = {
+  ALLOWED_ORIGINS: ["http://localhost:3000", process.env.APP_URL],
   PORT: process.env.PORT || 8080,
   SITE_ID: process.env.SITE_ID,
   URLS,
--- server.js
+++ server.js
@@ -2,12 +2,28 @@ const express = require("express");
 const logger = require("morgan");
 const itemsRouter = require("./routes/items");
 const app = express();
-const { PORT } = require("./constants");
+const cors = require("cors");
+const { ALLOWED_ORIGINS, PORT } = require("./constants");
 
 app.use(logger("dev"));
 app.use(express.json());
 // In this case, the below line is optional
 app.use(express.urlencoded({ extended: false }));
+
+// Enable cors
+app.use(
+  cors({
+    origin: (origin, cb) => {
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if (!origin) return cb(null, true);
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        return cb(new Error("You have been blocked by CORS policy"), false);
+      }
+      return cb(null, true);
+    },
+  })
+);
 
 app.use("/api", itemsRouter);
 app.use("/", (_, res) => res.sendStatus(404));
